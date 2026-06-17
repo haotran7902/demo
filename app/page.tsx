242a0814a768fafc8e9b162ab63b29501022ad37
Hao <tranhaosplus@gmail.com>
2026-06-17 21:03:14 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <header>
+        <h1> NEO playground </h1>
+        <ul>
+          <li><a href="#"> Home </a></li>
+          <li><a href="#"> Neo </a></li>
+          <li><a href="#"> Playground </a></li>
+        </ul>
+      </header>
+    </>
+  );
+}
